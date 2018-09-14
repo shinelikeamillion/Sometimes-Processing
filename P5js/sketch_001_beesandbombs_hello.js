2124b1ca64200207f15bc80accec61b92c26246e
lilsean <geeks.liu@gmail.com>
2018-09-14 21:28:19 +0800

    beesandbombs_hello

--- P5js/sketch_001_beesandbombs_hello.js
+++ P5js/sketch_001_beesandbombs_hello.js
@@ -2,8 +2,8 @@ var theta = 0
 var lines = 9
 var maxSize = 40
 var numberOfCircle
-var gap = maxSize/3
-var startY
+var gap = maxSize/2
+var startY;
 function setup() {
   createCanvas(windowWidth, windowHeight)
   noFill()
@@ -16,11 +16,11 @@ function draw() {
   background(0)
   for(var i = 0; i < lines; i++) {
     for(var j = 1; j < numberOfCircle-1; j++) {
-      var size = map(cos(j*0.25-theta+(i % 2 == 0 ? 0 : PI)), 1, -1, 1, maxSize)
+      var size = map(cos(j*0.25+theta+(i % 2 == 0 ? 0 : PI)), 1, -1, 1, maxSize)
       ellipse(gap*j, startY+maxSize*i*0.8, size, size)
     }
   }
-  theta += 0.02
+  theta+= 0.02
 }
 
 function windowResized() {
