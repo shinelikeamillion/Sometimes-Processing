8c7d8485022f88ef3005840c2eab9d9c8802d31d
lilsean <geeks.liu@gmail.com>
2018-11-07 23:37:30 +0800

    rename and add some comments

--- P5js/sketch_008_eyes_on_you.js
+++ P5js/sketch_008_eyes_on_you.js
@@ -1,12 +1,16 @@
 /**
  * from http://processingjs.org/learning/basic/arctangent/
- * 的 JS5 版本
+ * 的 P5.js 版本
+ * 一些技巧，Eyes 的绘制只和自己的接收的参数有关
  */
-function Eyes(ex, ey, angle) {
+
+function Eyes(ex, ey, size, angle) {
     this.ex = ex;
     this.ey = ey;
+    this.size = size;
     this.angle = angle;
     this.update = function(mx, my) {
+        // 此函数返回一个角度，避免了相减值为负的情况
         angle = atan2(my-ey, mx-ex);
     };
     this.display = function() {
@@ -21,20 +25,18 @@ function Eyes(ex, ey, angle) {
     }
 }
 
-var size;
 var eyes = new Array();
-var space;
+var space = 8;
 function setup() {
     createCanvas(400, 400);
-    space = 8;
-    size = 400 / 10 - space;
+    var size = 400 / 10 - space;
     smooth();
     noStroke();
 
     for(var i = 0; i < 10; i++) {
         eyes[i] = new Array()
         for(var j = 0; j< 10; j++) {
-            eyes[i][j] = new Eyes(i*(size + space) + size/2, j*(size + space) + size/2, 0);
+            eyes[i][j] = new Eyes(i*(size + space) + size/2, j*(size + space) + size/2, size, 0);
         }
     }
 }
