588d395412f22bbbf383dfbe093ed490707be648
lilsean <geeks.liu@gmail.com>
2019-04-06 18:37:32 +0800

    add dylan

--- P5js/sketch_018_dylan.js
+++ P5js/sketch_018_dylan.js
@@ -42,16 +42,24 @@ function setup() {
 	// strokeWeight(1.5)
   smooth(8)
   noFill()
-  noLoop(); // preload img or img will not show
+  // noLoop(); // preload img or img will not show
   imgSize = img.width < img.height ? img.width : img.height;
   img.loadPixels()
 }
+
+var theta = 0;
 // todo 区域内平均色值； 动态大小
 function draw() {
+  theta += 0.2;
   background(mouseIsPressed?50:0);
   // testColorPicker()
   // test
   // style3.show(width/2, height/2)
+  size = map(sin(theta), -1, 1, 0, 8)
+ gap = size/5
+ canvasSize = 620
+ nums = parseInt(canvasSize / (size + gap))
+padding = (canvasSize - nums * (size + gap) + gap) / 2
   for(i = 0; i < nums; i++){
     let y = i * (size + gap) + size/2 + padding
     let iy = parseInt(map(y, 0, height, 0, imgSize))
