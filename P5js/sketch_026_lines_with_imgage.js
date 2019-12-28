3fbeac4bb0acf4f91412283e42ea731ce2a30302
lilsean <geeks.liu@gmail.com>
2019-12-29 02:00:33 +0800

    lines with image

---
+++ P5js/sketch_026_lines_with_imgage.js
@@ -0,0 +1,20 @@
+
+step = 8
+rate = 0
+preload=_=> {
+    bg = loadImage('http://localhost:5500/P5js/sources/dylan.jpg')
+}
+setup=draw=_=>{
+    createCanvas(500,500)
+    rate = bg.width < bg.height ? bg.width/width : bg.height/height
+    noLoop()
+    
+    for (let y = 0; y < height; y+=step) {
+        beginShape()
+        for(let x = 0; x < width; x+=step){
+            b = brightness(bg.get(x * rate, y * rate))
+            vertex(x, y - map(b, 0, 100, step, 0))
+        }
+        endShape()
+    }
+}
