2a0f9bb476222845d91f415713f433d5d6a0f715
lilsean <geeks.liu@gmail.com>
2019-12-29 03:20:02 +0800

    fix empty data

--- P5js/sketch_028_fill_with_imgage.js
+++ P5js/sketch_028_fill_with_imgage.js
@@ -4,24 +4,26 @@ rate = 0
 preload=_=> {
     bg = loadImage('http://localhost:5500/P5js/sources/dylan.jpg')
 }
-setup=draw=_=>{
+setup=_=>{
     createCanvas(500,500)
     rate = bg.width < bg.height ? bg.width/width : bg.height/height
     noLoop()
     noStroke()
     fill(0)
-    
+}
+draw=_=>{
     for (let y = 0; y < height; y+=step) {
-        var b = []
         beginShape()
+        b = []
+        // x/step 应该很好理解，x+= step 会造成很多空数据
         for(let x = 0; x < width; x+=step){
-            b[x] = map(brightness(bg.get(x * rate, y * rate)), 0, 100, step/2, 0)
+            b[x/step] = map(brightness(bg.get(x * rate, y * rate)), 0, 100, step/2, 0)
             
-            vertex(x, y - b[x])
+            vertex(x, y - b[x/step])
             // >= 至关重要( 为 ！（x < width）)
             if(x+step >= width){
                 for(let x1 = x; x1 > 0; x1-=step){
-                    vertex(x1, y + b[x1])
+                    vertex(x1, y + b[x1/step])
                 }
             }
         }
