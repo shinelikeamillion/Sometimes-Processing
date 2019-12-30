4f6a4d85295eb1991eb0812454c5d8a368a444c7
lilsean <geeks.liu@gmail.com>
2019-12-31 01:10:16 +0800

    dylan with quil

---
+++ P5js/sketch_037_dylan_with_quil.js
@@ -0,0 +1,22 @@
+
+preload=_=> {
+    bg = loadImage('http://localhost:5500/P5js/sources/dylan.jpg')
+}
+setup=_=>{
+    createCanvas(500,500)
+    rate = bg.width < bg.height ? bg.width/width : bg.height/height
+    // noLoop()
+    fill(0)
+}
+
+draw=_=>{
+    translate(width/2, height/2)
+    for(let a = 0; a<frameCount/2; a+=0.02){
+        x = a * sin(a)
+        y = a * cos(a)
+        b = map(brightness(bg.get((width/2+x) * rate, (width/2+y) * rate)), 0, 100, 4, 0)
+        strokeWeight(b)
+        point(x, y)
+        if(x < -width/2 || x > width/2) noLoop()
+    }
+}
