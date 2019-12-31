d50cc6a9d3d9af5e7ccf01d454c04bfe2be07283
lilsean <geeks.liu@gmail.com>
2019-12-31 18:54:36 +0800

    jodeus' wave

---
+++ P5js/sketch_038_wave_of_jodeus.js
@@ -0,0 +1,21 @@
+// Original: https://jodeus.tumblr.com/page/6
+
+setup=_=>{
+    createCanvas(200,200)
+    num = 12
+    maxSize = width/num
+    fill(0)
+    // noLoop()
+}
+
+draw=_=>{
+    background(255)
+    for(let y = 1; y<num; y++){
+        for(let x = 1; x<num; x++){
+            speedX = frameCount/140*x
+            speedY = 1/6*y
+            size = map(sin(frameCount/4  + (speedX+speedY)*PI), -1, 1, maxSize/2, maxSize)
+            ellipse(x*maxSize, y*maxSize, size, size)
+        }
+    }
+}
