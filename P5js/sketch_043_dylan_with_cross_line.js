838a26c73b7cc244a9604a3246d7264d54a8e9eb
lilsean <geeks.liu@gmail.com>
2020-01-07 00:41:34 +0800

    曲线

--- P5js/sketch_043_dylan_with_cross_line.js
+++ P5js/sketch_043_dylan_with_cross_line.js
@@ -15,7 +15,7 @@ setup=draw=_=>{
         for(let x = 4; x < width; x+=step){
             b = brightness(bg.get(x * rate, y * rate))
             if(b >= 70) continue
-            offSize = map(b, 0, 100, step*2, 1)
+            offSize = map(b, 0, 70, step*2, 1, true)
             h = offSize * cos(PI/3) * 2/3
             line(x, y, x + h * cos(a), y + h*sin(a))
             line(x, y, x + h * cos(a+4*PI/3), y + h*sin(a+4*PI/3))
---
+++ P5js/sketch_060_lou_with_circle.js
@@ -0,0 +1,59 @@
+
+step = 9
+rate = 0
+preload = _ => {
+    bg = loadImage('http://localhost:5500/sources/lou.jpg')
+}
+setup = _ => {
+    createCanvas(500, 500)
+    noFill()
+    stroke(color(255, 40, 100))
+    noLoop()
+    rate = bg.width < bg.height ? bg.width / width : bg.height / height
+}
+
+draw = _ => {
+    image(bg, 0, 0, 500, bg.height / rate)
+    translate(width / 2, height / 2)
+
+    
+    for (let r = step; r < width/2; r += 4) {
+
+        off = TWO_PI / (pow(floor(r / step / 2), 2) + 100)
+
+        start = end = 0
+        first = true
+        for (let a = 0; a <= TWO_PI; a += off) {
+            // 保证闭合
+            if (TWO_PI - a < off && TWO_PI - a > 0) a = TWO_PI - off
+
+            x = (r * cos(a) + width / 2) * rate
+            y = (r * sin(a) + height / 2) * rate
+            b = brightness(bg.get(x, y))
+            if (b < 60) {
+                if (first && start == 0 && end == 0) {
+                    start = a
+                    first = false
+                    push()
+                    stroke(255)
+                    ellipse(r * cos(a), r * sin(a), 5)
+                    pop()
+                }
+                end = a
+            }
+            if ((b >= 60 || end == TWO_PI) && !first) {
+                if(start == end) end += 0.05
+                // push()
+                // stroke(0)
+                // ellipse(r * cos(a), r * sin(a), 5)
+                // pop()
+
+
+                arc(0, 0, 2 * r, 2 * r, start, end)
+                start = 0
+                end = 0
+                first = true
+            }
+        }
+    }
+}
