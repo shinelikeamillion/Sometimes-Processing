a9b1d42d681eef2e7aaf22238c3704dc03ce4251
lilsean <geeks.liu@gmail.com>
2020-01-06 15:01:00 +0800

    refactory

--- P5js/sketch_049_dylan_with_curve_lines.js
+++ P5js/sketch_049_dylan_with_curve_lines.js
@@ -1,16 +1,18 @@
 
-step = 8
+step = 9
 rate = 0
-preload=_=> {
+preload=_=>{
     bg = loadImage('http://localhost:5500/sources/dylan.jpg')
 }
-draw=_=>{
+setup=_=> {
     createCanvas(500,500)
     noFill()
     stroke(color(255, 40, 100))
-    rate = bg.width < bg.height ? bg.width/width : bg.height/height
     noLoop()
-    
+    rate = bg.width < bg.height ? bg.width/width : bg.height/height
+}
+draw=_=>{
+    stroke(color(255, 40, 100))
     for (let y = step/2; y < height; y+=step) {
         beginShape()
         for(let x = 0; x < width; x+=1){
@@ -18,7 +20,6 @@ draw=_=>{
             // true 限制超过边界的值
             offH = map(b, 0, 70, step/2, 0, true)
             offR = map(b, 70, 100, 1, TWO_PI, true)
-            print(b, offR)
             off = map(sin(x/offR), -1, 1, -offH, offH)
             vertex(x, y + off)
         }
