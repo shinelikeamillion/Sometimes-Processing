87fce5a8f31b8efdcd258c6c4734796392298a04
lilsean <geeks.liu@gmail.com>
2020-01-06 14:52:48 +0800

    使用第五个参数限制边界 使用亮度控制波浪线的振幅和周期

---
+++ P5js/sketch_049_dylan_with_curve_lines.js
@@ -0,0 +1,27 @@
+
+step = 8
+rate = 0
+preload=_=> {
+    bg = loadImage('http://localhost:5500/sources/dylan.jpg')
+}
+draw=_=>{
+    createCanvas(500,500)
+    noFill()
+    stroke(color(255, 40, 100))
+    rate = bg.width < bg.height ? bg.width/width : bg.height/height
+    noLoop()
+    
+    for (let y = step/2; y < height; y+=step) {
+        beginShape()
+        for(let x = 0; x < width; x+=1){
+            b = brightness(bg.get(x * rate, y * rate))
+            // true 限制超过边界的值
+            offH = map(b, 0, 70, step/2, 0, true)
+            offR = map(b, 70, 100, 1, TWO_PI, true)
+            print(b, offR)
+            off = map(sin(x/offR), -1, 1, -offH, offH)
+            vertex(x, y + off)
+        }
+        endShape()
+    }
+}
