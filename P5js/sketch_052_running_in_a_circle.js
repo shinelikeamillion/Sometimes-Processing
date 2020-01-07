08bbe153f2983bbe8679c44d016ad25aff22ec70
lilsean <geeks.liu@gmail.com>
2020-01-07 19:34:28 +0800

    running in a circle

---
+++ P5js/sketch_052_running_in_a_circle.js
@@ -0,0 +1,48 @@
+
+
+setup = _ => {
+    createCanvas(300, 300)
+    noFill()
+    strokeWeight(2)
+    // noLoop()
+}
+
+draw = _ => {
+    background(0)
+    translate(width / 2, height / 2)
+
+    if (frameCount % 2 == 0) {
+        stroke(255)
+        ellipse(0, 0, width/2)
+    }
+    t = floor(frameCount % 360)*2
+    for (let i = 0; i < 1; i++) {
+        start = t + i * 30
+        end = t + 180 + i * 30
+
+        stroke(color(255, 40, 100))
+        beginShape()
+        for (let a = t; a <= end + 180; a += 1) {
+            //  弧度模式
+            // angleMode(RADIANS)
+            if (a > start && a < end) {
+                off = 8 - abs(map(a, start, end, -8, 8))
+                r = map(sin((a + frameCount / 20) * 11), -1, 1, width/4 - off, width/4 + off)
+            } else {
+                r = width/4
+            }
+
+            // 角度模式
+            angleMode(DEGREES)
+            x = r * cos(a)
+            y = r * sin(a)
+            // ellipse(x, y,5)
+            vertex(x, y)
+        }
+        endShape()
+    }
+    if (frameCount % 2 == 1) {
+        stroke(255)
+        ellipse(0, 0, width/2)
+    }
+}
