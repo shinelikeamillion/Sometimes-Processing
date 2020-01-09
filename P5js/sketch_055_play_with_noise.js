f48bef26727de6eb289bf58b972f1ffa3252ad27
lilsean <geeks.liu@gmail.com>
2020-01-09 20:48:53 +0800

    play with noise

---
+++ P5js/sketch_055_play_with_noise.js
@@ -0,0 +1,27 @@
+size = 20
+noiseS = 0.02
+gap = 2
+setup = _ => {
+    createCanvas(500, 500)
+    background(175)
+    rectMode(CENTER)
+    noStroke()
+    num = floor(width / (size + gap))
+    margin = (width % (size + gap)) / 2
+}
+draw = _ => {
+    for (i = 0; i < num; i++) {
+        for (j = 0; j < num; j++) {
+            // 调整噪音函数生成噪音特征及细节度
+            noiseDetail(2, 0.9)
+            noiseV = noise((mouseX + j) * noiseS,
+                (mouseY + i) * noiseS)
+
+            x = margin + i * (size + gap) + (size + gap) / 2
+            y = margin + j * (size + gap) + (size + gap) / 2
+
+            fill(255 * noiseV)
+            rect(x, y, size, size)
+        }
+    }
+}
