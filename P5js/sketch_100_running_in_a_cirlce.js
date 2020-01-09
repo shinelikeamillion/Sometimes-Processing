414f8d5746fc4ca28763d7f007a9d36ab75b87fa
lilsean <geeks.liu@gmail.com>
2020-01-09 19:13:47 +0800

    normaterial

--- P5js/sketch_100_running_in_a_cirlce.js
+++ P5js/sketch_100_running_in_a_cirlce.js
@@ -1,25 +1,27 @@
 let capture
 setup = _ => {
     createCanvas(568, 568, WEBGL)
-    // normalMaterial()
+    normalMaterial()
     noStroke()
-    fill(220)
+    // fill(220)
     // noLoop()
 
     cs = []
-    for (i = 0; i <20; i++) {
+    for (i = 0; i <34; i++) {
         cs.push(new C(i))
     }
 }
 
+// 周期为 20
 function C (num) {
     this.inside  = true
     this.num = num
     this.count = 1
     this.show = function(){
-        if(frameCount < this.num*6) return
+        // 控制间隔
+        if(frameCount < this.num*5.9) return
         push()
-        // 使用 PI 抵消掉周期误差
+        // 使用 PI 抵消掉周期误差1
         // off = sin(++this.count*PI/100-HALF_PI)
         this.inside = (this.count % 20 - 10)  < 0
         // 使用线性插值
@@ -28,26 +30,28 @@ function C (num) {
         this.size = this.inside ? map(abs(off), 0, 1, 10, 30) : 60 * sin(map(abs(off), 0, 1, PI/2, PI/6))
         translate(0, 0, z)
         if (this.size < 12) stroke(color(255, 40, 100))
+        if (this.num == 33) stroke(255)
         torus(this.size, 2)
         pop()
+        // 控制速度
         this.count+= 0.1
     }
 }
 
 draw = _ => {
-    // rotateX(HALF_PI+frameCount/30)
-    // rotateY(HALF_PI+frameCount/30)
+    // rotateX(HALF_PI+frameCount/50)
+    // rotateY(HALF_PI+frameCount/50)
     rotateX(QUARTER_PI)
     rotateY(QUARTER_PI)
     background(0)
 
     for(let i = 0; i < cs.length; i++){
         cs[i].show()
-        print(i, cs[i].count > frameCount)
     }
     if(capture !== undefined) capture.cap()
 }
 
 mousePressed = _ => {
-    // capture = new Capture(this, frameCount+200)
+    capture = new Capture(this, frameCount+200)
+    // noLoop()
 }
